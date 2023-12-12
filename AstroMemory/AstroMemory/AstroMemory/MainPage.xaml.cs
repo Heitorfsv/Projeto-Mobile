2b7c3d28fcc9be4eef063fad42b67c077a7ad8e0
Heitor Vieira <heitorfsv@gmail.com>
2023-12-12 20:29:02 -0300

    Projeto mais recente

---
+++ AstroMemory/AstroMemory/AstroMemory/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xamarin.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace AstroMemory
+{
+    public partial class MainPage : ContentPage
+    {
+        URL imagem = new URL();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = imagem;
+        }
+
+        public string API_key = "QQnn5JJ17EubXsHTv5bT03J1dfrLcuSDFUfdu1NX";
+        private HttpClient _client = new HttpClient();
+
+        public void clicado(object sender, EventArgs args)
+        {
+            DateTime selectedDate = puxar_data.Date;
+            string formattedDate = selectedDate.ToString("yyyy-MM-dd");
+            PuxarDados(API_key, formattedDate);
+        }
+
+        async Task PuxarDados(string key, string data)
+        {
+            try
+            {
+                string APIUrl = String.Format("https://api.nasa.gov/planetary/apod?api_key={0}&date={1}", key, data);
+                HttpResponseMessage response = await _client.GetAsync(APIUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    JObject jsonObject = JObject.Parse(json);
+                    URL imagem = new URL();
+                    imagem.url = jsonObject["url"].ToString();
+                    BindingContext = imagem;
+                }
+                else
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
+
+
---
+++ AstroMemory/AstroMemory/AstroMemory/URL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AstroMemory
+{
+    public class URL
+    {
+        public string url { get; set; } = "nasa_foto.png";
+    }
+}
